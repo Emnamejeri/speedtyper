61da19af919c23e5a0b068b6b31c58f389a6d1fc
Emna Mejeri <107746809+Emnamejeri@users.noreply.github.com>
2023-08-13 18:07:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,17 +27,23 @@ function updateDisplayedText() {
   textContainer.innerHTML = formattedPhrase;
 }
 
-function displayTime() {
+//handle the event lsiteners
+function setUpTestInteractions() {
   document.getElementById("actionStart").addEventListener("click", startTest);
   document.getElementById("testInput").addEventListener("input", userInput);
-
   window.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      startTest();
-    } else if (event.key === "Escape") {
+    if (event.key === "Escape") {
       resetTest();
+      startCountdown(60);
     }
   });
+  document.getElementById("testInput").disabled = true;
+  const storedTypingSpeed = localStorage.getItem("typingSpeed");
+  const storedTypingAccuracy = localStorage.getItem("typingAccuracy");
+
+  if (storedTypingSpeed && storedTypingAccuracy) {
+    displayUserStats(storedTypingSpeed, storedTypingAccuracy);
+  }
 }
 
 function startTest() {
@@ -55,9 +61,46 @@ function startTest() {
     }, 60 * 1000);
   }
 }
+function userInput() {
+  if (isTimerRunning) {
+    const userInputField = document.getElementById("testInput");
+    const userInput = userInputField.value;
+    const displayedChars = document.querySelectorAll(".char");
 
+    for (let i = 0; i < displayedChars.length; i++) {
+      const char = displayedChars[i];
+      if (i < userInput.length) {
+        colorUpdate(char, userInput[i], expectedText[currentIndex + i]);
+      } else {
+        colorUpdate(char, "", expectedText[currentIndex + i]);
+      }
+    }
+    //to update the displayed text every 100 charac
+    if (userInput.length >= 100) {
+      currentIndex += 100;
+
+      if (currentIndex % 100 === 0) {
+        updateDisplayedText();
+      }
+    }
+
+    if (userInput.length >= expectedText.length && !isTimerRunning) {
+      calculateTypingSpeed();
+    }
+  }
+}
+
+function colorUpdate(element, userChar, expectedChar) {
+  if (userChar === expectedChar) {
+    element.style.backgroundColor = "#1FAB89";
+  } else if (userChar === "") {
+    element.style.backgroundColor = "transparent";
+  } else {
+    element.style.backgroundColor = "#FF395E";
+  }
+}
+// Create a new button element
 function addNewButton() {
-  // Create a new button element
   const newButton = document.createElement("button");
   newButton.textContent = "Try again";
   newButton.classList.add("time-remaining-box");
@@ -69,7 +112,7 @@ function addNewButton() {
   buttonContainer.appendChild(newButton);
 }
 
-// To reload the page and start over
+// To reload the page and start over again
 function resetTest() {
   clearInterval(timerInterval);
   isTimerRunning = false;
@@ -77,7 +120,21 @@ function resetTest() {
   document.getElementById("testInput").value = "";
   fetchContent();
   document.getElementById("testInput").disabled = false;
-  document.querySelector(".square-style").innerHTML = "";
+
+  // Clear the results display
+  const squareStyleElement = document.querySelector(".square-style");
+  squareStyleElement.style.display = "none";
+
+  // Reset the timer display
+  const timerElement = document.getElementById("timer");
+  timerElement.textContent = "60"; // Set it back to the initial value
+
+  // Remove the "Try again" button if it exists
+  const buttonContainer = document.getElementsByClassName("buttonContainer")[0];
+  const tryAgainButton = document.querySelector(".time-remaining-box");
+  if (tryAgainButton) {
+    buttonContainer.removeChild(tryAgainButton);
+  }
 }
 
 function updateTimerMessage(message) {
@@ -107,7 +164,7 @@ function startCountdown(seconds) {
 
 //To calculate user's typing speed and accuracy with comments
 function calculateTypingSpeed() {
-  const userInput = document.getElementById("testInput").value.trim();
+  const userInput = document.getElementById("testInput").value;
   let correctCharacters = 0;
   let totalTypedCharacters = 0;
 
@@ -115,25 +172,22 @@ function calculateTypingSpeed() {
     const userChar = userInput[i];
     const expectedChar = expectedText[currentIndex + i];
 
-    // Consider only valid user input characters (excluding backspaces)
-    if (userChar !== "") {
-      totalTypedCharacters++;
+    totalTypedCharacters++;
 
-      if (userChar === expectedChar) {
-        correctCharacters++;
-      }
+    if (userChar === expectedChar) {
+      correctCharacters++;
     }
   }
-
+  console.log(totalTypedCharacters);
   const typingAccuracy = (correctCharacters / totalTypedCharacters) * 100;
-  const typingSpeedWPM = (totalTypedCharacters / 5) * 12;
+  const typingSpeedWPM = totalTypedCharacters / 5;
 
   // Determine the message based on user's typing speed
   let message = "";
   if (typingSpeedWPM <= 30) {
     message = "You Type like a Turtle 🐢";
   } else if (typingSpeedWPM <= 40) {
-    message = "You Type like a Grandma 👵🏼";
+    message = "You Type like Grandma 👵🏼";
   } else if (typingSpeedWPM <= 55) {
     message = "You Type like a Programmer 👩🏻‍💻";
   } else {
@@ -141,6 +195,9 @@ function calculateTypingSpeed() {
   }
 
   displayResults(typingSpeedWPM, typingAccuracy, message);
+  localStorage.setItem("typingSpeed", typingSpeedWPM);
+  localStorage.setItem("typingAccuracy", typingAccuracy.toFixed(0));
+  displayUserStats(typingSpeedWPM, typingAccuracy.toFixed(0));
 }
 
 function displayResults(typingSpeedWPM, typingAccuracy, message) {
@@ -153,40 +210,5 @@ function displayResults(typingSpeedWPM, typingAccuracy, message) {
   squareStyleElement.style.display = "block";
 }
 
-function userInput() {
-  if (isTimerRunning) {
-    const userInputField = document.getElementById("testInput");
-    const userInput = userInputField.value;
-    const displayedChars = document.querySelectorAll(".char");
+setUpTestInteractions();
 
-    for (let i = 0; i < displayedChars.length; i++) {
-      const char = displayedChars[i];
-      if (i < userInput.length) {
-        colorUpdate(char, userInput[i], expectedText[currentIndex + i]);
-      } else {
-        colorUpdate(char, "", expectedText[currentIndex + i]);
-      }
-    }
-
-    if (userInput.length >= 100) {
-      currentIndex += 100;
-      userInputField.value = "";
-      updateDisplayedText();
-    }
-    if (userInput.length >= expectedText.length && !isTimerRunning) {
-      calculateTypingSpeed();
-    }
-  }
-}
-
-function colorUpdate(element, userChar, expectedChar) {
-  if (userChar === expectedChar) {
-    element.style.backgroundColor = "#1FAB89";
-  } else if (userChar === "") {
-    element.style.backgroundColor = "transparent";
-  } else {
-    element.style.backgroundColor = "#FF395E";
-  }
-}
-
-displayTime();
